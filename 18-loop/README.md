23485585e891d68c7fc3431c9a17c5b4c4eac7ac
Wallys Ferreira <wallysfsilva@gmail.com>
2024-03-04 16:56:00 -0300

    video 19

---
+++ 18-loop/README.md
@@ -0,0 +1,4 @@
+# Looping
+
+Jumping instructions can also be used to go back to some previous instruction.
+This makes it possible to create loops, usually jumping back if a condition is met or not.
---
+++ 18-loop/first.asm
@@ -0,0 +1,21 @@
+section .data
+  list DB 1,3,4,6
+
+section .text
+global _start
+
+_start:
+  mov eax,0
+  mov cl,0
+
+loop:
+  mov bl,[list + eax]
+  add cl,bl
+  inc eax
+  cmp eax,4
+  jne loop
+
+end:
+  mov eax,1
+  mov ebx,1
+  int 0x80
